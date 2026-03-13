18ec08ddc21b5d7ad9fe7ea4b7c015002321fd0b
ManojKumarTS27 <manojkumar27ts@gmail.com>
2026-03-13 18:32:54 +0530

    Calculator

---
+++ calculator.js
@@ -0,0 +1,20 @@
+let num1 = Number(prompt("Enter first number: "));
+let num2 = Number(prompt("Enter second number: "));
+
+let calculator = prompt("Choose any one(+, -, *, /): ");
+
+if (calculator == "+") {
+    console.log("Result:", num1 + num2);
+}
+else if (calculator == "-") {
+    console.log("Result:", num1 - num2);
+}
+else if (calculator == "*") {
+    console.log("Result:", num1 * num2);
+}
+else if (calculator == "/") {
+    console.log("Result:", num1 / num2);
+}
+else {
+    console.log("Invalid operation");
+}
