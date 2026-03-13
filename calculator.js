8c7afbfb441e80c03e6185934628dbff524bed09
ManojKumarTS27 <manojkumar27ts@gmail.com>
2026-03-13 18:41:21 +0530

    Calculator

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
-let num1 =Number(prompt("Enter first number"));
-let num2 =Number(prompt("Enter second number"));
+let num1=Number(prompt("Enter first number"));
+let num2=Number(prompt("Enter second number"));
 
 let calculator =prompt("Choose any one(+, -, *, /): ");
 
